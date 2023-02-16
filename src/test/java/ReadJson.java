a4f076f9b48048adfd3741668b4f38f9fc32af77
Dheeraj2709 <awsdjdemo@gmail.com>
2023-02-16 16:00:21 +0530

    final commit

--- src/test/java/ReadJson.java
+++ src/test/java/ReadJson.java
@@ -8,17 +8,54 @@ public class ReadJson {
 
     @Test
     public void readjson() {
-        JSONParser parser = new JSONParser();
+        int count=0;
+        int foreigncount =0;
+
         try {
+            JSONParser parser = new JSONParser();
             Object obj = parser.parse(new FileReader("TestJson.json"));
             JSONObject jsonObject = (JSONObject) obj;
             JSONArray subjects = (JSONArray) jsonObject.get("player");
-            Iterator iterator = subjects.iterator();
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next());
+            int jsonarraysize = ((JSONArray) jsonObject.get("player")).size();
+
+            for(int i=0;i<jsonarraysize;i++)
+            {
+                JSONObject records = (JSONObject)  subjects.get(i);
+                if(records.get("role").toString().equals("Wicket-keeper"))
+                    count=count+1;
+                if(!records.get("country").toString().equals("India"))
+                    foreigncount=foreigncount+1;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+          /*  Iterator iterator = subjects.iterator();
+
+            while (iterator.hasNext())
+            {
+
+                System.out.println(iterator.next());
+
+                if(iterator.next().toString().contains("Wicket-keeper"))
+                {
+                    count=count+1;
+                }
+                if(!(iterator.next().toString().contains("India")))
+                {
+                    foreigncount=foreigncount+1;
+                }
+            }*/
+            
+            if(count==1)
+                System.out.println("There is only one Wicket keeper in list");
+            else
+                System.out.println("There is more than one Wicket keeper in list");
+
+            if(foreigncount==4)
+                System.out.println("There is only 4 foreign players in list");
+            else
+                System.out.println("There is more than 4 foreign players in list");
+
+        }
+        catch (Exception e) {
+            System.out.println("There is some error in list");
         }
     }
 }
